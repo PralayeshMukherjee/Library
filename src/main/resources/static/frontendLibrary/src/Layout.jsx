8441294cbf16fb15a42a4226a625258bc406d444
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-10 20:25:46 +0530

    use outlet component from react-router-dom to iterate different component

--- src/main/resources/static/frontendLibrary/src/Layout.jsx
+++ src/main/resources/static/frontendLibrary/src/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Component/Landing/Header'
 import Footer from './Component/Landing/Footer'
+import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
     <>
         <Header/>
+        <Outlet/>
         <Footer/>
     </>
   )
