9cdd99a45c27395ffc9ab02a02375d59e2d1634f
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-10 22:21:28 +0530

    add the mapping when user click the login button

--- src/main/resources/static/frontendLibrary/src/Component/Landing/Header.jsx
+++ src/main/resources/static/frontendLibrary/src/Component/Landing/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="#"
+                            to="/userLogin"
                             className="text-white hover:bg-gray-50 hover:text-black focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
