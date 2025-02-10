c3290b5b10377193aab71bbc83dcb629048055a0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-10 19:32:14 +0530

    allowed mapping,methods and headers and here I only allowed a temporary origin which I change latter

--- src/main/java/com/raj/library/Config/CORSConfig.java
+++ src/main/java/com/raj/library/Config/CORSConfig.java
@@ -1,4 +1,23 @@
 package com.raj.library.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer config(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") //allowed all end points
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET","POST","PUT","DELETE")//allowed methods
+                        .allowedHeaders("*");//all headers allowed
+            }
+        };
+    }
 }
