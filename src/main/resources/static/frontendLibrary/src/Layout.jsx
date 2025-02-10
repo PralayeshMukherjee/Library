4d852cbddee37da955dc661053e34c490b0db656
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-10 20:21:20 +0530

    add a Layout file from where my elements iterate

---
+++ src/main/resources/static/frontendLibrary/src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Component/Landing/Header'
+import Footer from './Component/Landing/Footer'
+
+function Layout() {
+  return (
+    <>
+        <Header/>
+        <Footer/>
+    </>
+  )
+}
+
+export default Layout
