8f6b7d84c4f8c7780e0ff6c9714a08ecd5f5f33c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-10 20:20:45 +0530

    add new router in this file for routing in jsx files

--- src/main/resources/static/frontendLibrary/src/App.jsx
+++ src/main/resources/static/frontendLibrary/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import {Layout} from './Component/index'
 
 function App() {
   const [count, setCount] = useState(0)
+  const MyRouter = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout/>} >
 
+      </Route>
+    )
+  )
   return (
     <>
-      
+      <RouterProvider router={MyRouter} />
     </>
   )
 }
