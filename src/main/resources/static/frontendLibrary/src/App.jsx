2b3ea32c25484426fc701d926ebffcbe5c8ae3d5
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-10 22:21:39 +0530

    add a new route for user login

--- src/main/resources/static/frontendLibrary/src/App.jsx
+++ src/main/resources/static/frontendLibrary/src/App.jsx
@@ -1,13 +1,16 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import {Layout,Home} from './Component/index'
+import {Layout,Home,UserLogin} from './Component/index'
 
 function App() {
   const MyRouter = createBrowserRouter(
     createRoutesFromElements(
+      <>
       <Route path='/' element={<Layout/>} >
         <Route index element={<Home/>}/>//for when / hit the url then home page automaticly loaded
         <Route path='home' element={<Home/>}/>
       </Route>
+      <Route path='userLogin' element={<UserLogin/>}/>
+      </>
     )
   )
   return (
