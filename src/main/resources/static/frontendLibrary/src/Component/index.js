26d4d2e92df96c84880966be3846c265d1ca4e3a
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-11 18:01:25 +0530

    import the user register file function

--- src/main/resources/static/frontendLibrary/src/Component/index.js
+++ src/main/resources/static/frontendLibrary/src/Component/index.js
@@ -1,5 +1,6 @@
 import Layout from '../Layout'
 import Home from './Landing/Home'
 import UserLogin from './LORE/Login/UserLogin'
+import UserRegister from '../Component/LORE/Register/UserRegister'
 
-export {Layout,Home,UserLogin};
+export {Layout,Home,UserLogin,UserRegister};
