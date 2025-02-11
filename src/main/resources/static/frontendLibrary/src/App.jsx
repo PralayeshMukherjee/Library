33361244dfe68f719ee022981bcd3b56de9c37cb
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-12 00:08:21 +0530

    add the registration successful router

--- src/main/resources/static/frontendLibrary/src/App.jsx
+++ src/main/resources/static/frontendLibrary/src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import {Layout,Home,UserLogin,UserRegister} from './Component/index'
+import {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess} from './Component/index'
 
 function App() {
   const MyRouter = createBrowserRouter(
@@ -9,8 +9,9 @@ function App() {
         <Route index element={<Home/>}/>//for when / hit the url then home page automaticly loaded
         <Route path='home' element={<Home/>}/>
       </Route>
-      <Route path='userLogin' element={<UserLogin/>}/>
-      <Route path='userRegister' element={<UserRegister/>}/>
+      <Route path='/userLogin' element={<UserLogin/>}/>
+      <Route path='/userRegister' element={<UserRegister/>}/>
+      <Route path='/UserRegisterSuccess' element={<UserRegisterSuccess/>}/>
       </>
     )
   )
