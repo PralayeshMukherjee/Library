be444a5800dba7c99ac0b2aa333b1ac0a52d745a
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-12 00:08:43 +0530

    import the registration successful component

--- src/main/resources/static/frontendLibrary/src/Component/index.js
+++ src/main/resources/static/frontendLibrary/src/Component/index.js
@@ -2,5 +2,6 @@ import Layout from '../Layout'
 import Home from './Landing/Home'
 import UserLogin from './LORE/Login/UserLogin'
 import UserRegister from '../Component/LORE/Register/UserRegister'
+import UserRegisterSuccess from '../Component/Success/UserRegisterSuccess'
 
-export {Layout,Home,UserLogin,UserRegister};
+export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess};
