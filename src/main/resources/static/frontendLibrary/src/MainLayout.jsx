40966b967ad87e417ffcea5bd9f27aac3c050492
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-12 23:35:37 +0530

    add the main footer component in main layout

--- src/main/resources/static/frontendLibrary/src/MainLayout.jsx
+++ src/main/resources/static/frontendLibrary/src/MainLayout.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { MainHeader } from "./Component/index";
+import { MainHeader, MainFooter } from "./Component/index";
 function MainLayout() {
-  return <MainHeader />;
+  return (
+    <>
+      <MainHeader />
+      <MainFooter />
+    </>
+  );
 }
 
 export default MainLayout;
