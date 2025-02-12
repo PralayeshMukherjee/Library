8d309810fb3da7f897b68a5c0b668108cb56c727
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-13 00:55:45 +0530

    import all the images folder files and export all

--- src/main/resources/static/frontendLibrary/src/Component/index.js
+++ src/main/resources/static/frontendLibrary/src/Component/index.js
@@ -8,5 +8,14 @@ import MainLayout from '../MainLayout'
 import MainFooter from '../Component/MainPages/MainFooter.jsx'
 import MainHome from '../Component/MainPages/MainHome.jsx'
 import About from '../Component/Landing/About.jsx'
+import Library from "../images/Library.png";
+import User from "../images/user.png";
+import Profile from "../images/profile.png";
+import Setting from "../images/setting.png";
+import Help from "../images/help.png";
+import Logout from "../images/logout.png";
+import Pic from "../images/pic.png";
+import userLoginPNG from "../images/userLogin.jpg";
+import AboutPNG from '../images/About.svg'
 
-export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess,MainHeader,MainLayout,MainFooter,MainHome,About};
+export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess,MainHeader,MainLayout,MainFooter,MainHome,About,Library,User,Profile,Setting,Help,Logout,Pic,userLoginPNG,AboutPNG};
