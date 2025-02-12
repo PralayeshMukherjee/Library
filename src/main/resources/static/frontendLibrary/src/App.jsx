fb81c240685377130e69a475da081a176b5d212c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-12 23:02:28 +0530

    add a new router for main header

--- src/main/resources/static/frontendLibrary/src/App.jsx
+++ src/main/resources/static/frontendLibrary/src/App.jsx
@@ -1,25 +1,40 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess} from './Component/index'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import {
+  Layout,
+  Home,
+  UserLogin,
+  UserRegister,
+  UserRegisterSuccess,
+  MainHeader,
+  MainLayout,
+} from "./Component/index";
 
 function App() {
   const MyRouter = createBrowserRouter(
     createRoutesFromElements(
       <>
-      <Route path='/' element={<Layout/>} >
-        <Route index element={<Home/>}/>//for when / hit the url then home page automaticly loaded
-        <Route path='home' element={<Home/>}/>
-      </Route>
-      <Route path='/userLogin' element={<UserLogin/>}/>
-      <Route path='/userRegister' element={<UserRegister/>}/>
-      <Route path='/UserRegisterSuccess' element={<UserRegisterSuccess/>}/>
+        <Route path="/Landing" element={<Layout />}>
+          <Route index element={<Home />} />
+          //for when / hit the url then home page automaticly loaded
+          <Route path="home" element={<Home />} />
+        </Route>
+        <Route path="/userLogin" element={<UserLogin />} />
+        <Route path="/userRegister" element={<UserRegister />} />
+        <Route path="/UserRegisterSuccess" element={<UserRegisterSuccess />} />
+        <Route path="/Main" element={<MainLayout />}></Route>
       </>
     )
-  )
+  );
   return (
     <>
       <RouterProvider router={MyRouter} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
