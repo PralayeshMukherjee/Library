ce004695f065c3b35edef558a6272af7a0541f9e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-13 00:35:13 +0530

    add the route to load the about component

--- src/main/resources/static/frontendLibrary/src/App.jsx
+++ src/main/resources/static/frontendLibrary/src/App.jsx
@@ -14,6 +14,7 @@ import {
   MainLayout,
   MainFooter,
   MainHome,
+  About,
 } from "./Component/index";
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
           <Route index element={<Home />} />
           //for when / hit the url then home page automaticly loaded
           <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
         </Route>
         <Route path="/userLogin" element={<UserLogin />} />
         <Route path="/userRegister" element={<UserRegister />} />
@@ -31,6 +33,7 @@ function App() {
         <Route path="/Main" element={<MainLayout />}>
           <Route index element={<MainHome />} />
           <Route path="MainHome" element={<MainHome />} />
+          <Route path="about" element={<About />} />
         </Route>
       </>
     )
