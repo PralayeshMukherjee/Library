d62b7fd3f2ae62b5c05c3680f9c71dd3f54833f0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-12 23:03:46 +0530

    navigate to main router file

--- src/main/resources/static/frontendLibrary/src/Component/LORE/Login/UserLogin.jsx
+++ src/main/resources/static/frontendLibrary/src/Component/LORE/Login/UserLogin.jsx
@@ -1,51 +1,97 @@
-import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
-import userLogin from '../../../images/userLogin.jpg'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import userLogin from "../../../images/userLogin.jpg";
 
 function UserLogin() {
-    const [formData,setFormData] = useState({username:"",password:""});
-    const handelChange = (e)=>{
-        setFormData({...formData,[e.target.name]:e.target.value});
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const navigate = useNavigate();
+  const handelChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    const response = await fetch("http://localhost:8080/login/userLogin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    console.log(data);
+    if (data.exists) {
+      alert("user present here");
+      navigate("/Main");
+    } else {
+      alert("user not present");
     }
-    const handelSubmit = async (e) =>{
-        e.preventDefault();
-        console.log(formData);
-        
-        const response = await fetch('http://localhost:8080/login/userLogin',{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData)
-        });
-        const data = await response.json();
-        console.log(data);
-        if(data.exists){
-            alert("user present here");
-        }else{
-            alert("user not present");
-        }
-    }
+  };
   return (
-    <div className="flex justify-center items-center h-screen bg-cover bg-center" style={{ backgroundImage: `url(${userLogin})`}}>
+    <div
+      className="flex justify-center items-center h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${userLogin})` }}
+    >
       <div className="w-80 p-6 bg-white/20 bg-opacity-20 backdrop-blur-md shadow-md rounded-lg border border-white">
-        <h2 className="text-white text-xl font-semibold text-center mb-4">User Login</h2>
+        <h2 className="text-white text-xl font-semibold text-center mb-4">
+          User Login
+        </h2>
         <form onSubmit={handelSubmit} className="flex flex-col space-y-4">
           <div>
-            <label htmlFor="username" className="text-white block mb-1">Username:</label>
-            <input type="text" id="username" name="username" className="w-full p-2 bg-transparent border border-white text-white rounded-lg focus:outline-none placeholder-white" placeholder="Enter username" onChange={handelChange} />
+            <label htmlFor="username" className="text-white block mb-1">
+              Username:
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="w-full p-2 bg-transparent border border-white text-white rounded-lg focus:outline-none placeholder-white"
+              placeholder="Enter username"
+              onChange={handelChange}
+            />
           </div>
           <div>
-            <label htmlFor="password" className="text-white block mb-1">Password:</label>
-            <input type="password" id="password" name="password" className="w-full p-2 bg-transparent border border-white text-white rounded-lg focus:outline-none placeholder-white" placeholder="Enter password" onChange={handelChange} />
+            <label htmlFor="password" className="text-white block mb-1">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full p-2 bg-transparent border border-white text-white rounded-lg focus:outline-none placeholder-white"
+              placeholder="Enter password"
+              onChange={handelChange}
+            />
           </div>
-          <button type="submit" className="bg-white text-black py-2 rounded-lg hover:bg-transparent hover:text-white border border-white transition">Login</button>
-          <p className="text-white text-xs text-center">Don't have an account? <Link to='/userRegister' className="text-orange-400 hover:text-orange-500">Register as User</Link></p>
-          <p className="text-white text-xs text-center">Login as Admin: <Link href="/login/admin-Login" className="text-orange-400 hover:text-orange-500">Login as Admin</Link></p>
+          <button
+            type="submit"
+            className="bg-white text-black py-2 rounded-lg hover:bg-transparent hover:text-white border border-white transition"
+          >
+            Login
+          </button>
+          <p className="text-white text-xs text-center">
+            Don't have an account?{" "}
+            <Link
+              to="/userRegister"
+              className="text-orange-400 hover:text-orange-500"
+            >
+              Register as User
+            </Link>
+          </p>
+          <p className="text-white text-xs text-center">
+            Login as Admin:{" "}
+            <Link
+              href="/login/admin-Login"
+              className="text-orange-400 hover:text-orange-500"
+            >
+              Login as Admin
+            </Link>
+          </p>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default UserLogin
+export default UserLogin;
