cbdddf8353b6eed7348539d3f7658a16c32e42ee
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-12 23:01:41 +0530

    import the main header file

--- src/main/resources/static/frontendLibrary/src/Component/index.js
+++ src/main/resources/static/frontendLibrary/src/Component/index.js
@@ -3,5 +3,7 @@ import Home from './Landing/Home'
 import UserLogin from './LORE/Login/UserLogin'
 import UserRegister from '../Component/LORE/Register/UserRegister'
 import UserRegisterSuccess from '../Component/Success/UserRegisterSuccess'
+import MainHeader from '../Component/MainPages/MainHeader'
+import MainLayout from '../MainLayout'
 
-export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess};
+export {Layout,Home,UserLogin,UserRegister,UserRegisterSuccess,MainHeader,MainLayout};
