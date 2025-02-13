af6402890b31b0ec0b3324adceb83b3f312e01e3
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-13 17:47:05 +0530

    add fields like id,title,author,isbn and a list of sellers which is mapped by books and also add getter & setter methods an constructors

--- src/main/java/com/raj/library/entity/Book.java
+++ src/main/java/com/raj/library/entity/Book.java
@@ -1,7 +1,71 @@
 package com.raj.library.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "Book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+
+    @ManyToMany(mappedBy = "books")
+    private List<Seller> sellers = new ArrayList<>();
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String author, String isbn, List<Seller> sellers) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.sellers = sellers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public List<Seller> getSellers() {
+        return sellers;
+    }
+
+    public void setSellers(List<Seller> sellers) {
+        this.sellers = sellers;
+    }
 }
