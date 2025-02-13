1621c26473a48d6d581c4632421d68894c89512b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-13 18:19:42 +0530

    create a file that use as Seller repository which Extends Jpa Repository which gives me CRUD options and use JPQL to write query

---
+++ src/main/java/com/raj/library/repository/SellerRepository.java
@@ -0,0 +1,15 @@
+package com.raj.library.repository;
+
+import com.raj.library.entity.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface SellerRepository extends JpaRepository<Seller,Long> {
+//    add a JPQL to use query
+    @Query("SELECT s FROM Seller s JOIN s.books b WHERE b.title LIKE %:title%")
+    List<Seller> findSellersByBookTitle(@Param("title")String title);
+}
