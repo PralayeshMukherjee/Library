8f02db75b82c4dd2f9b8d5b766d84d571697e74e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-13 18:45:38 +0530

    add a service method to fetch sellers from the database and return the list of sellers

--- src/main/java/com/raj/library/Service/FeatureService.java
+++ src/main/java/com/raj/library/Service/FeatureService.java
@@ -1,7 +1,19 @@
 package com.raj.library.Service;
 
+import com.raj.library.entity.Seller;
+import com.raj.library.repository.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FeatureService {
+    @Autowired
+    private SellerRepository sellerRepository;
+    public ResponseEntity<List<Seller>> searchSellersByBook(String title){
+        List<Seller> sellers = sellerRepository.findSellersByBookTitle(title);
+        return ResponseEntity.ok(sellers);
+    }
 }
