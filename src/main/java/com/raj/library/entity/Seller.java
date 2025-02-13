bcb55c9fcd4989aa5d4eceec66814ae8ec70f1c5
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-13 17:00:39 +0530

    remove age and add id is my primary key

--- src/main/java/com/raj/library/entity/Seller.java
+++ src/main/java/com/raj/library/entity/Seller.java
@@ -2,29 +2,34 @@ package com.raj.library.entity;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name="Seller")
 public class Seller {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String userName;
     private String password;
     private String name;
-    private int age;
     private String phoneNumber;
     private String emailId;
 
     public Seller(){
 
     }
-    public Seller(String userName, String password, String name, int age, String phoneNumber, String emailId) {
-        this.userName = userName;
-        this.password = password;
-        this.name = name;
-        this.age = age;
-        this.phoneNumber = phoneNumber;
+
+    public Seller(String emailId, String phoneNumber, String name, String password, String userName, Long id) {
         this.emailId = emailId;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+        this.password = password;
+        this.userName = userName;
+        this.id = id;
     }
-//    I use Many to Many annotation from JPA repo because seller has multiple books and books also have multiple seller it automatically manage two tables
+
+    //    I use Many to Many annotation from JPA repo because seller has multiple books and books also have multiple seller it automatically manage two tables
     @ManyToMany
 //    to use this mapping we need a intermediate (junction) table to connect two entities
 //    the @ManyToMany does not create the Join table so we need to create this
@@ -34,6 +39,14 @@ public class Seller {
             inverseJoinColumns = @JoinColumn(name = "book_id")//foreign key (FK) referring to Book
     )
 //    use join table method to join seller table and book table with seller_book table where seller_id from seller table is the foreign key to refer seller and book_id from book table is the foreign key to refer book
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUserName() {
         return userName;
@@ -57,14 +70,6 @@ public class Seller {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public String getPhoneNumber() {
