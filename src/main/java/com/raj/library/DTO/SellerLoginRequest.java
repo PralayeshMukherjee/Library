ea462b05bdb43c1a77afbd4924d200ae38a13b6e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-15 00:52:45 +0530

    add the getter and setter methods

--- src/main/java/com/raj/library/DTO/SellerLoginRequest.java
+++ src/main/java/com/raj/library/DTO/SellerLoginRequest.java
@@ -9,4 +9,44 @@ public class SellerLoginRequest {
 
     public SellerLoginRequest() {
     }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
 }
