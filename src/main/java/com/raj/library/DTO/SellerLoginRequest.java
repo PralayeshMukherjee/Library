79c08908fe12824e733b9894a2c6773416093d07
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-15 00:51:41 +0530

    add field of Seller login request

--- src/main/java/com/raj/library/DTO/SellerLoginRequest.java
+++ src/main/java/com/raj/library/DTO/SellerLoginRequest.java
@@ -1,4 +1,11 @@
 package com.raj.library.DTO;
 
 public class SellerLoginRequest {
+    private String userName;
+    private String password;
+    private String name;
+    private String phoneNumber;
+    private String emailId;
+
+
 }
