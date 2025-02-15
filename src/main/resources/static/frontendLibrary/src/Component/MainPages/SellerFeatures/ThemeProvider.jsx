413b1bcca8a6543ee1f075b66cbfbe69cc6fe858
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-15 23:56:38 +0530

    add a new file for themeprovider

---
+++ src/main/resources/static/frontendLibrary/src/Component/MainPages/SellerFeatures/ThemeProvider.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <div className={darkMode ? "dark" : "light"}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeProvider, ThemeContext };
