66a8c6a04501fd6c56599d4d2ab789f2089ae312
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-15 17:17:39 +0530

    add all the fields

--- src/main/java/com/raj/library/DTO/RegisterSeller.java
+++ src/main/java/com/raj/library/DTO/RegisterSeller.java
@@ -1,4 +1,9 @@
 package com.raj.library.DTO;
 
 public class RegisterSeller {
+    private String userName;
+    private String password;
+    private String name;
+    private String phoneNumber;
+    private String emailId;
 }
