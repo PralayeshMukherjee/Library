eba1515f16f05be51973976ab8d84ecac31d642c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-15 17:59:47 +0530

    remove all comment out code

--- src/main/java/com/raj/library/Service/UserService.java
+++ src/main/java/com/raj/library/Service/UserService.java
@@ -28,30 +28,6 @@ public class UserService {
             return false;
         }
     }
-//    public boolean checkUserName(User user){
-//        if(userRepo.existsById(user.getUsername())){
-//            return true;
-//        }else {
-//            return false;
-//        }
-////        return userRepo.existsById(username);
-//    }
-//    private String userUserName;
-//    public boolean checkPassword(User user){
-//        Optional<User> byId = userRepo.findById(user.getUsername());
-//        System.out.println(byId.get().getPassword());
-//        System.out.println(user.getPassword());
-//        if(byId.get().getPassword().equals(user.getPassword())){
-//            userUserName = byId.get().getUsername();
-//            return true;
-//        }else{
-//            return false;
-//        }
-//    }
-//    public User getUser(){
-//        Optional<User> byId = userRepo.findById(userUserName);
-//        return byId.get();
-//    }
     public boolean userCheckerService(String username, String password){
         try {
             System.out.println(username);
