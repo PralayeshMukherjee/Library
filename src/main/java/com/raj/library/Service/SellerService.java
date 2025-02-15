ab3da9d211e025d16eb1b9ba6ef9c18ce005d86e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-15 17:59:34 +0530

    is user exist then its return true else return false

--- src/main/java/com/raj/library/Service/SellerService.java
+++ src/main/java/com/raj/library/Service/SellerService.java
@@ -29,31 +29,17 @@ public class SellerService {
         }
     }
 
-//    public void addAdmin(Seller admin){
-//        adminRepo.save(admin);
-//    }
-//
-//    public boolean checkUserName(Seller admin){
-//        if(adminRepo.existsById(admin.getUserName())){
-//            return true;
-//        }else{
-//            return false;
-//        }
-//    }
-//    private String adminUserName;
-//    public boolean checkPassword(Seller admin){
-//        Optional<Seller> byId = adminRepo.findById(admin.getUserName());
-//        System.out.println(byId.get().getPassword());
-//        System.out.println(admin.getPassword());
-//        if(byId.get().getPassword().equals(admin.getPassword())){
-//            adminUserName = byId.get().getUserName();
-//            return true;
-//        }else{
-//            return false;
-//        }
-//    }
-//    public Seller getAdmin(){
-//        Optional<Seller> byId = adminRepo.findById(adminUserName);
-//        return byId.get();
-//    }
+    public boolean userCheckerService(String userName,String password){
+        try {
+            Seller temp = sellerRepo.findByUsernameAndPassword(userName,password);
+            if(temp!=null){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return  false;
+        }
+    }
 }
