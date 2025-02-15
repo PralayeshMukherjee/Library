4541ec80c49a599cf884dd770e1db40124c0e79b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-02-15 19:13:34 +0530

    change the method name because in database UserName colum exist

--- src/main/java/com/raj/library/repository/SellerRepository.java
+++ src/main/java/com/raj/library/repository/SellerRepository.java
@@ -2,15 +2,12 @@ package com.raj.library.repository;
 
 import com.raj.library.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 @Repository
 public interface SellerRepository extends JpaRepository<Seller,Long> {
 //    add a JPQL to use query
-    @Query("SELECT s FROM Seller s JOIN s.books b WHERE b.title LIKE %:title%")
-    List<Seller> findSellersByBookTitle(@Param("title")String title);
-    Seller findByUsernameAndPassword(String userName,String password);
+//    @Query("SELECT s FROM Seller s JOIN s.books b WHERE b.title LIKE %:title%")
+//    List<Seller> findSellersByBookTitle(@Param("title")String title);
+    Seller findByUserNameAndPassword(String userName, String password);
 }
